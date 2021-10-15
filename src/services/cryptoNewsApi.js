582d3c643628a3ec203cd7eb4c6c25990b6c027e
suprunov <rocknroll2418@gmail.com>
2021-10-15 12:02:29 +0300

    News Api

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import {cryptoApi} from "./cryptoApi";
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '3bd50207bbmshd336dda4055792fp1e73dajsna6ed5bbfc45c'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders})
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+})
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi
